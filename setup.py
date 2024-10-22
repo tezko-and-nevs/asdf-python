f2a0ed83de4cd0abee50964e09ef462e7ae9db0f
tezko-and-nevs <tezko-and-nevs@users.noreply.github.com>
2024-10-22 19:37:48 +0000

    Merge branch dev of https://github.com/nukeop/SteamRelayBot into dev

--- setup.py
+++ setup.py
@@ -1,1 +1,3 @@
 from setuptools import setup, find_packages\n\nsetup(\n    name='asdf-python',\n    version='1.0.0',\n    packages=find_packages(),\n    install_requires=[]\n)
+
+# Touch update: 1761209119
